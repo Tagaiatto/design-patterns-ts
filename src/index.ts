5d01e36c826650fa6dce7b361c4c8223d397785d
Edilson Mendes <edilsonmneto@gmail.com>
2023-03-29 00:17:01 -0300

    factory pattern

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { RecordHandler, loader } from "./loader";
 interface Pokemon {
     id: string,
     attack: number,
-    defence: number
+    defense: number
 }
 
 interface BaseRecord {
@@ -11,19 +11,37 @@ interface BaseRecord {
 }
 
 interface Database<T> {
-    set(newValue: T): void
+    set(newValue: T): void;
     get(id: string): T | undefined;
 }
 
-class InMemoryDatabase<T extends BaseRecord> implements Database<T> {
-    private db: Record<string, T> = {};
+//factory pattern
+function createDatabase<T extends BaseRecord>(){
+    class InMemoryDatabase implements Database<T> {
+        private db: Record<string, T> = {};
+        
+        public set(newValue: T): void {
+            this.db[newValue.id] = newValue;
+        }
     
-    public set(newValue: T): void {
-        this.db[newValue.id] = newValue;
+        public get(id: string): T | undefined {
+            return this.db[id];
+        }
+    
     }
-
-    public get(id: string): T | undefined {
-        return this.db[id];
-    }
-
+    
+    return InMemoryDatabase;
 }
+
+
+
+const PokemonDB = createDatabase<Pokemon>();
+const pokemonDB = new PokemonDB();
+
+pokemonDB.set({
+    id: 'Bulbasaur',
+    attack: 50,
+    defense: 10,
+});
+
+console.log(pokemonDB.get('Bulbasaur'))
