d68eeb3ab58156a92b7f5460214f8e55d211dc4b
Edilson Mendes <edilsonmneto@gmail.com>
2023-03-30 22:01:50 -0300

    event listener

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,35 @@
 import { RecordHandler, loader } from "./loader";
 
+//Observer
+type Listener<EventType> = (event: EventType) => void;
+function createObserver<EventType>():{
+    subscribe: (listener: Listener<EventType>) => () => void;
+    publish: (event: EventType) => void;
+} {
+    let listeners: Listener<EventType>[] = [];
+
+    return {
+        subscribe: (listener: Listener<EventType>): () => void => {
+            listeners.push(listener);
+            return () => {
+                listeners = listeners.filter(l => l !== listener)
+            }
+        },
+        publish(event: EventType) {
+            listeners.forEach((l) => l(event));
+        } 
+    }
+}
+
+interface BeforeSetEvent<T> {
+    value: T;
+    newValue: T;
+}
+
+interface AfterSetEvent<T> {
+    value: T;
+}
+
 interface Pokemon {
     id: string,
     attack: number,
@@ -13,6 +43,9 @@ interface BaseRecord {
 interface Database<T> {
     set(newValue: T): void;
     get(id: string): T | undefined;
+
+    onBeforeAdd(listener: Listener<BeforeSetEvent<T>>): () => void;
+    onAfterAdd(listener: Listener<AfterSetEvent<T>>): () => void;
 }
 
 //factory pattern
@@ -22,17 +55,36 @@ function createDatabase<T extends BaseRecord>(){
         
         static instance: InMemoryDatabase = new InMemoryDatabase();
 
+        private beforeAddListeners = createObserver<BeforeSetEvent<T>>();
+        private afterAddListeners = createObserver<AfterSetEvent<T>>();
+
         private constructor() {}
 
         public set(newValue: T): void {
+            this.beforeAddListeners.publish({
+                newValue,
+                value: this.db[newValue.id]
+            })
             this.db[newValue.id] = newValue;
+
+            this.afterAddListeners.publish({
+                value: newValue
+            })
         }
     
         public get(id: string): T | undefined {
             return this.db[id];
         }
+
+        onBeforeAdd(listener: Listener<BeforeSetEvent<T>>): () => void {
+            return this.beforeAddListeners.subscribe(listener);
+        };
+        onAfterAdd(listener: Listener<AfterSetEvent<T>>): () => void {
+            return this.afterAddListeners.subscribe(listener);
+        };
     
     }
+
     //Singleton
     // const db = new InMemoryDatabase();
     // return db;
